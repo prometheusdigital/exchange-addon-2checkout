652ead0d35abd1947ece698966cf072184344e08
Lew Ayotte <lew@ithemes.com>
2015-06-24 09:00:18 -0500

    Adding deactivation warning

--- exchange-addon-2checkout.php
+++ exchange-addon-2checkout.php
@@ -77,5 +77,19 @@ function ithemes_exchange_addon_2checkout_updater_register( $updater ) {
 
 }
 add_action( 'ithemes_updater_register', 'ithemes_exchange_addon_2checkout_updater_register' );
-
 require( dirname( __FILE__ ) . '/lib/updater/load.php' );
+
+function ithemes_exchange_2checkout_deactivate() {
+	if ( empty( $_GET['remove-gateway'] ) || 'yes' !== $_GET['remove-gateway'] ) {
+		$title = __( 'Payment Gateway Warning', 'LION' );
+		$yes = '<a href="' . esc_url( add_query_arg( 'remove-gateway', 'yes' ) ) . '">' . __( 'Yes', 'LION' ) . '</a>';
+		$no  = '<a href="javascript:history.back()">' . __( 'No', 'LION' ) . '</a>';
+		$message = '<p>' . sprintf( __( 'Deactivating a payment gateway can cause customers to lose access to any membership products they have purchased. Are you sure you want to proceed? %s | %s', 'LION' ), $yes, $no ) . '</p>';
+		$args = array(
+			'response'  => 200,
+			'back_link' => false,
+		);
+		wp_die( $message, $title, $args );
+	}
+}
+register_deactivation_hook( __FILE__, 'ithemes_exchange_2checkout_deactivate' );
