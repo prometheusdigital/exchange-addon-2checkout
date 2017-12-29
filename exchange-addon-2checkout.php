11e91d6386111897ce6f3aa46f53313b02c27881
AJ Morris <ajmorris2002@gmail.com>
2017-12-29 01:30:48 -0500

    Update to include the new update all access license mechanism.

--- exchange-addon-2checkout.php
+++ exchange-addon-2checkout.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: ExchangeWP - 2Checkout Add-on
- * Version: 1.2.2
+ * Version: 1.2.1
  * Description: Adds the ability for users to checkout with 2Checkout.
  * Plugin URI: https://exchangewp.com/downloads/2checkout/
  * Author: ExchangeWP
@@ -94,29 +94,28 @@ function ithemes_exchange_2checkout_deactivate() {
 }
 register_deactivation_hook( __FILE__, 'ithemes_exchange_2checkout_deactivate' );
 
-if ( ! class_exists( 'EDD_SL_Plugin_Updater' ) )  {
-	require_once 'EDD_SL_Plugin_Updater.php';
-}
-
 function exchange_2checkout_plugin_updater() {
+	$license_data = get_transient( 'exchangewp_license_check' );
 
-	// retrieve our license key from the DB
-	// this is going to have to be pulled from a seralized array to get the actual key.
-	// $license_key = trim( get_option( 'exchange_2checkout_license_key' ) );
-	$exchangewp_2checkout_options = get_option( 'it-storage-exchange_addon_2checkout' );
-	$license_key = $exchangewp_2checkout_options['2checkout_license'];
+	if ( $license_data->license == 'valid' ) {
+		// retrieve our license key from the DB
+		// this is going to have to be pulled from a seralized array to get the actual key.
+		// $license_key = trim( get_option( 'exchange_2checkout_license_key' ) );
+		$exchangewp_license = it_exchange_get_option( 'exchangewp_licenses' );
+		$license = $exchangewp_license['exchangewp_license'];
 
-	// setup the updater
-	$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
-			'version' 		=> '1.2.2', 				// current version number
-			'license' 		=> $license_key, 		// license key (used get_option above to retrieve from DB)
-			'item_name' 	=> '2checkout', 	  // name of this plugin
-			'author' 	  	=> 'ExchangeWP',    // author of this plugin
-			'url'       	=> home_url(),
-			'wp_override' => true,
-			'beta'		  	=> false
-		)
-	);
+		// setup the updater
+		$edd_updater = new EDD_SL_Plugin_Updater( 'https://exchangewp.com', __FILE__, array(
+				'version' 		=> '1.2.1', 				// current version number
+				'license' 		=> $license, 		// license key (used get_option above to retrieve from DB)
+				'item_name' 	=> '2checkout', 	  // name of this plugin
+				'author' 	  	=> 'ExchangeWP',    // author of this plugin
+				'url'       	=> home_url(),
+				'wp_override' => true,
+				'beta'		  	=> false
+			)
+		);
+	}
 	// var_dump($edd_updater);
 	// die();
 
